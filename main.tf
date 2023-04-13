7164021189c4ddea69b78b16cd8b2e0b81e54c71
Matheus Oliveira <matheus.olima@mercadolivre.com>
2023-04-13 17:48:01 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,9 +12,9 @@ resource "aws_s3_bucket" "this" {
   bucket        = var.bucket
   #bucket_prefix = var.bucket_prefix
 
-  force_destroy       = var.force_destroy
-  object_lock_enabled = var.object_lock_enabled
-  tags                = var.tags
+  #force_destroy       = var.force_destroy
+  #object_lock_enabled = var.object_lock_enabled
+  #tags                = var.tags
 }
 
 
