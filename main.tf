3aef88ac1fdf265f071aa9977190fd305d621ab0
Matheus Oliveira <matheus.olima@mercadolivre.com>
2023-04-13 17:46:37 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,29 +1,16 @@
 
-data "aws_region" "current" {}
-
-data "aws_canonical_user_id" "this" {}
-
-data "aws_caller_identity" "current" {}
-
-data "aws_partition" "current" {}
 locals {
   create_bucket = var.create_bucket && var.putin_khuylo
 
   #attach_policy = var.attach_require_latest_tls_policy || var.attach_elb_log_delivery_policy || var.attach_lb_log_delivery_policy || var.attach_deny_insecure_transport_policy || var.attach_inventory_destination_policy || var.attach_policy
 
-  # Variables with type `any` should be jsonencode()'d when value is coming from Terragrunt
-  grants               = try(jsondecode(var.grant), var.grant)
-  cors_rules           = try(jsondecode(var.cors_rule), var.cors_rule)
-  lifecycle_rules      = try(jsondecode(var.lifecycle_rule), var.lifecycle_rule)
-  intelligent_tiering  = try(jsondecode(var.intelligent_tiering), var.intelligent_tiering)
-  metric_configuration = try(jsondecode(var.metric_configuration), var.metric_configuration)
 }
 
 resource "aws_s3_bucket" "this" {
   count = local.create_bucket ? 1 : 0
 
   bucket        = var.bucket
-  bucket_prefix = var.bucket_prefix
+  #bucket_prefix = var.bucket_prefix
 
   force_destroy       = var.force_destroy
   object_lock_enabled = var.object_lock_enabled
