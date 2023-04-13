d2281a7397fc04aa4f705b5a3e18a752939763ae
Matheus Oliveira <matheus.olima@mercadolivre.com>
2023-04-13 15:38:18 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,6 +27,15 @@ resource "aws_s3_bucket" "this" {
   #force_destroy       = var.force_destroy
   #object_lock_enabled = var.object_lock_enabled
   tags                = var.tags
+  
+  lifecycle_rule{
+    id     = "Incomplete multi-part uploads"
+    status = "Enabled"
+
+    abort_incomplete_multipart_upload {
+      days_after_initiation = 8
+    }
+  }
 }
 
 resource "aws_s3_bucket_logging" "this" {
